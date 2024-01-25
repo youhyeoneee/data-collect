0e56bf00fa733d3a6736e7bdca595df753b0dbf8
youhyeoneee <khukyh2018@khu.ac.kr>
2024-01-25 16:52:35 +0900

    feat: 2페이지까지 수집

--- quotesToScrape.js
+++ quotesToScrape.js
@@ -1,26 +1,32 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 
-const url = "https://quotes.toscrape.com";
+const baseUrl = "https://quotes.toscrape.com/";
 const results = [];
+const maxPage = 2;
 
-axios.get(url).then((response) => {
-    const $ = cheerio.load(response.data);
-    const quotes = $("div.quote")
-        .map((i, el) => {
-            const target = $(el);
-            const tags = [];
-            target.find(".tag").map((i, el) => {
-                tags.push($(el).text());
-            });
+for (let page = 1; page <= maxPage; page++) {
+    const url = baseUrl + `page/${page}`;
+    axios.get(url).then((response) => {
+        const $ = cheerio.load(response.data);
+        // console.log($.html());
+        const quotes = $("div.quote")
+            .map((i, el) => {
+                const target = $(el);
+                const tags = [];
+                target.find(".tag").map((i, el) => {
+                    tags.push($(el).text());
+                });
 
-            return {
-                quote: target.children(".text").text(),
-                author: target.find(".author").text(),
-                authorUrl: url + target.find("a").prop("href"),
-                tag: tags,
-            };
-        })
-        .get();
-    console.log(quotes);
-});
+                return {
+                    quote: target.children(".text").text(),
+                    author: target.find(".author").text(),
+                    authorUrl: url + target.find("a").prop("href"),
+                    tag: tags,
+                };
+            })
+            .get();
+        console.log(`${page} 페이지 ----------`);
+        console.log(quotes);
+    });
+}
