a19890e575e320f30ca37b922ca36634e133a326
youhyeoneee <khukyh2018@khu.ac.kr>
2024-01-30 14:48:31 +0900

    feat: 와디즈 이미지 수집

--- src/wadiz.js
+++ src/wadiz.js
@@ -16,6 +16,22 @@ async function fetchPage(url) {
     }
 }
 
+async function fetchFile(url) {
+    try {
+        const response = await axios.get(url, {
+            responseType: "stream",
+            headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+            },
+        });
+        return response;
+    } catch (error) {
+        console.error(`Error fetching page: ${url}`, error.message);
+        return null;
+    }
+}
+
 async function saveData(results) {
     fs.writeFileSync(filePath, JSON.stringify(results));
 }
@@ -23,8 +39,30 @@ async function saveData(results) {
 async function fetchMain(url) {
     const response = await fetchPage(url);
     if (response) {
-        await saveData(response.data.data["list"]);
+        let result = response.data.data["list"];
+
+        await Promise.all(
+            result.map(async (e, i) => {
+                e.photoPath = await getImage(e.photoUrl, e.campaignId);
+                console.log(e.photoPath);
+            })
+        );
+
+        console.log(result[0]);
+        await saveData(result);
     }
+}
+
+async function getImage(url, id) {
+    const response = await fetchFile(url);
+    let name = `../data/wadiz/${id}.jpeg`;
+    let file = fs.createWriteStream(name);
+
+    if (response) {
+        response.data.pipe(file);
+    }
+
+    return name;
 }
 
 const baseUrl = "https://service.wadiz.kr/api/search/funding";
